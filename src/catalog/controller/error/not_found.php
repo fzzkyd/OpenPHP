74b0c9fe255246a1f887fca311d9a5852989ca1d
fzzkyd <amogha@qq.com>
2019-07-08 23:46:02 +0800

    Update not_found.php

--- src/catalog/controller/error/not_found.php
+++ src/catalog/controller/error/not_found.php
@@ -3,6 +3,8 @@ class ControllerErrorNotFound extends Controller
 {
     public function index()
     {
-        // ...
+        $this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 404 Not Found');
+
+        $this->response->setOutput($this->load->view('error/not_found', $data));
     }
 }
