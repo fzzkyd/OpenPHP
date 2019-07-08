8a05e1126b079837617baea185ec73816032bb86
fzzkyd <amogha@qq.com>
2019-07-08 23:55:51 +0800

    Update code specification
    1. Optimized code specification

--- src/catalog/controller/common/home.php
+++ src/catalog/controller/common/home.php
@@ -1,8 +1,8 @@
 <?php
-class ControllerCommonHome extends Controller {
-	public function index() {
-        $data = array();
-        
-		$this->response->setOutput($this->load->view('common/home', $data));
-	}
+class ControllerCommonHome extends Controller
+{
+    public function index()
+    {
+        $this->response->setOutput($this->load->view('common/home'));
+    }
 }
