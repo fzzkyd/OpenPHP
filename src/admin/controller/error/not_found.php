e9c5716927f1e6c6f42a418e5676f1d719e969b1
fzzkyd <amogha@qq.com>
2019-07-09 22:36:53 +0800

    Update src\admin\controller\error\not_found.php

--- src/admin/controller/error/not_found.php
+++ src/admin/controller/error/not_found.php
@@ -3,6 +3,6 @@ class ControllerErrorNotFound extends Controller
 {
     public function index()
     {
-        // ...
+        $this->response->setOutput($this->load->view('error/not_found'));
     }
 }
